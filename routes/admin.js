f9280ac5c897e44b01cdc8c4bc3122ccd783ae18
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-06-27 01:03:25 +0530

    add db schrma

---
+++ routes/admin.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function (req, res) {
+  res.json({
+    message: "admin sign up",
+  });
+});
+adminRouter.post("/signin", function (req, res) {
+  res.json({
+    message: "admin sign in",
+  });
+});
+
+adminRouter.post("/course", function (req, res) {
+  res.json({
+    message: "admin course",
+  });
+});
+adminRouter.put("/course", function (req, res) {
+  res.json({
+    message: "admin course",
+  });
+});
+adminRouter.get("/course/all", function (req, res) {
+  res.json({
+    message: "admin course",
+  });
+});
+
+module.exports = {
+  adminRouter: adminRouter
+};
