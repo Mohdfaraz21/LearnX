2f67fbc80069a62f028a6decbb12b176ccd4b1cf
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-01 01:14:21 +0530

    added purchase ep for users

--- routes/course.js
+++ routes/course.js
@@ -1,18 +1,28 @@
 const { Router } = require("express");
+const { userMiddleware } = require("../middleware/user");
+const { purchaseModel, courseModel } = require("../db");
 const courseRouter = Router();
 
-courseRouter.post("/purchase", function(req, res) {
-    // you would expect to user can  give you money 
-    res.json({
-        message: "purchase endpoint"
+courseRouter.post("/purchase", userMiddleware, async function (req, res) {
+  const userId = req.userId;
+    const courseId = req.body.courseId;
+  // you would expect to user can  give you money
+    await purchaseModel.create({
+        userId,
+        courseId
     })
-})
-courseRouter.get("/preview", function(req, res) {
+  res.json({
+    message: "purchase endpoint",
+  });
+});
+courseRouter.get("/preview",async function (req, res) {
+ const course = await courseModel.find({});
+ 
     res.json({
-        message: "preview endpoint"
-    })
-})
+   course
+  });
+});
 
 module.exports = {
-    courseRouter : courseRouter
-}
+  courseRouter: courseRouter,
+};
