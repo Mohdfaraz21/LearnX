ed261a2885c07f075a827a1c33dcfe136c32bace
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-06-30 23:06:09 +0530

    try-catch and add logic in admin,user input

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,57 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = "admin250"
 
-adminRouter.post("/signup", function (req, res) {
-  res.json({
-    message: "admin sign up",
-  });
+adminRouter.post("/signup",async function (req, res) {
+  //TODO: Adding zod validation
+  const { email, password, firstName, lastName } = req.body;
+  //TODO: Hash the password with bcrypt so plain text password should not be in db
+
+  try {
+    await adminModel.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    res.status(201).json({
+      message: " Admin Signup successful",
+    });
+  } catch (e) {
+    console.error(" Signup error:", e.message);
+
+    res.status(500).json({
+      message: " Something went wrong during signup",
+      error: e.message,
+    });
+  }
 });
-adminRouter.post("/signin", function (req, res) {
-  res.json({
-    message: "admin sign in",
+adminRouter.post("/signin",async function (req, res) {
+    const { email, password } = req.body;
+ //TODO: ideally password should be hashed, hence you can't compare the user provided password and the db password.
+  const admin = await adminModel.findOne({
+    email: email,
+    password: password,
   });
+
+  if (admin) {
+    const token = jwt.sign(
+      {
+        id: admin._id,
+      },
+      JWT_ADMIN_PASSWORD
+    );
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "incorrect credentials"
+    });
+  }
 });
 
 adminRouter.post("/course", function (req, res) {
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,58 @@
 const { Router } = require("express");
+const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "user192";
+
 const userRouter = Router();
 
-userRouter.post("/signup", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
+userRouter.post("/signup", async function (req, res) {
+  //TODO: Adding zod validation
+  const { email, password, firstName, lastName } = req.body;
+  //TODO: Hash the password with bcrypt so plain text password should not be in db
+
+  try {
+    await userModel.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    res.status(201).json({
+      message: "✅ Signup successful",
+    });
+  } catch (e) {
+    console.error("❌ Signup error:", e.message);
+
+    res.status(500).json({
+      message: "❌ Something went wrong during signup",
+      error: e.message,
+    });
+  }
 });
-userRouter.post("/signin", function (req, res) {
-  res.json({
-    message: "signin endpoint",
+userRouter.post("/signin", async function (req, res) {
+  const { email, password } = req.body;
+ //TODO: ideally password should be hashed, hence you can't compare the user provided password and the db password.
+  const user = await userModel.findOne({
+    email: email,
+    password: password,
   });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      JWT_USER_PASSWORD
+    );
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "incorrect credentials"
+    });
+  }
 });
 userRouter.get("/purchases", function (req, res) {
   res.json({
@@ -18,5 +61,5 @@ userRouter.get("/purchases", function (req, res) {
 });
 
 module.exports = {
-  userRouter: userRouter
-}
+  userRouter: userRouter,
+};
