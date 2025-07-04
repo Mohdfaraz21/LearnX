c755b52a331580a4784a89d2cfeb5788e2e53020
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-05 01:00:25 +0530

    middleware added

--- ProductionGradeBackend/middleware/adminMiddleware.js
+++ ProductionGradeBackend/middleware/adminMiddleware.js
@@ -1,12 +1,12 @@
 function adminMiddleware(req, res, next) {
-    if(req.session && req.session.adminId) {
-        req.adminId = req.session.adminId;
-        return next();
-    }
+  if (req.session && req.session.adminId) {
+    req.adminId = req.session.adminId;
+    return next();
+  }
 
-    return res.status(401).json({message:"Unauthorized"});
+  return res.status(401).json({ message: "Unauthorized" });
 }
 
 module.exports = {
-    adminMiddleware,
+  adminMiddleware,
 };
